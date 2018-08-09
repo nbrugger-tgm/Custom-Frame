24950bfafbc0707e98bcc78143815c6a394d2835
Nils Brugger <34156963+nbrugger-tgm@users.noreply.github.com>
2018-08-09 21:35:54 +0200

    Coloring
    Defined Default Colors

---
+++ src/com/niton/themes/FlatDarkTheme.java
@@ -0,0 +1,4 @@
+	private Color border = new Color(66, 66, 66);
+	private Color content = new Color(95, 95, 95);
+	private Color borders = new Color(35, 35, 35);
+	private Color headerText = new Color(175, 175, 175);
