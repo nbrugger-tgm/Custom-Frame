4ca5734937479ad9a607f3b8c63685b524691829
Nils Brugger <34156963+nbrugger-tgm@users.noreply.github.com>
2018-08-09 21:24:45 +0200

    Formatting

--- src/com/niton/listeners/MoveListener.java
+++ src/com/niton/listeners/MoveListener.java
@@ -13,11 +13,13 @@ import java.awt.event.MouseEvent;
 
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 import com.niton.themes.base.ResizeableTheme;
 
 /**
  * This is the MoveListener Class
+ * 
  * @author Nils Brugger
  * @version 2018-08-09
  */
@@ -28,6 +30,7 @@ public class MoveListener extends MouseAdapter {
 	private boolean reresize = false;
 	private JFrame preview;
 	private Robot preventer;
+
 	public MoveListener(ResizeableTheme theme) {
 		super();
 		this.theme = theme;
@@ -36,7 +39,7 @@ public class MoveListener extends MouseAdapter {
 		preview.setOpacity(0.3f);
 		preview.setBackground(Color.WHITE);
 		preview.setType(Type.UTILITY);
-		preview.setSize(0,0);
+		preview.setSize(0, 0);
 		preview.setVisible(true);
 		preview.setAlwaysOnTop(true);
 		preview.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
@@ -46,43 +49,43 @@ public class MoveListener extends MouseAdapter {
 			e.printStackTrace();
 		}
 	}
+
 	@Override
 	public void mousePressed(MouseEvent e) {
+
+		if (e.getButton() != MouseEvent.BUTTON1)
+			return;
 		Point relative = e.getPoint();
-		if(
-				theme.getDragingArea().contains(relative) &&
-				!(
-						theme.getCloseArea().contains(relative) ||
-						theme.getMaximizeArea().contains(relative)||
-						theme.getMinimizeArea().contains(relative)
-				)
-		) {
+		if (theme.getDragingArea().contains(relative) && !(theme.getCloseArea().contains(relative)
+				|| theme.getMaximizeArea().contains(relative) || theme.getMinimizeArea().contains(relative))) {
 			Point absolute = e.getLocationOnScreen();
-			if(!theme.getFrame().isMaximized()) {
-				bevoreDrag = theme.getFrame().getSize();
-			}else {
-				reresize = true;
-			}
+			reresize = theme.getFrame().isMaximized();
 			lastPos = absolute;
-		}else {
+		} else {
 			lastPos = null;
 		}
 	}
+
 	public void mouseDragged(MouseEvent e) {
-		if(lastPos != null) {
-			if(reresize){
+		if (SwingUtilities.isRightMouseButton(e))
+			return;
+		if (lastPos != null) {
+			Point now = e.getLocationOnScreen();
+			if (reresize) {
 				reresize = false;
-				if(bevoreDrag != null)
-				theme.getFrame().setSize(bevoreDrag);
+				if (bevoreDrag != null)
+					theme.getFrame().setSize(bevoreDrag);
+				theme.getFrame().setLocation(now.x - (theme.getFrame().getWidth() / 2), (int) now.getY());
 				bevoreDrag = null;
 			}
-			Point now = e.getLocationOnScreen();
 			stayInArea(now, e);
 			moveWindow(now);
-			borderCheck(now,false);
+			borderCheck(now, false);
 			lastPos = now;
+			bevoreDrag = theme.getFrame().getSize();
 		}
 	}
+
 	/**
 	 * <b>Description :</b><br>
 	 * 
@@ -90,86 +93,88 @@ public class MoveListener extends MouseAdapter {
 	 * @version 2018-08-09
 	 * @param now
 	 */
-	private void borderCheck(Point now,boolean release) {
+	private void borderCheck(Point now, boolean release) {
 		Rectangle allowed = theme.getFrame().getMaximizedSize();
 		GrabPosition position = GrabPosition.BOTTOM;
 
 		Point bot = allowed.getLocation();
 		bot.translate(0, (int) allowed.getHeight());
-		if(bot.distance(now)<5) 
+		if (bot.distance(now) < 5)
 			position = GrabPosition.BOTTOM_LEFT;
-		else if(allowed.getLocation().distance(now)<5) 
+		else if (allowed.getLocation().distance(now) < 5)
 			position = GrabPosition.TOP_LEFT;
-		
+
 		Point topR = allowed.getLocation();
 		topR.translate((int) allowed.getWidth(), 0);
-		if(topR.distance(now)<5) 
+		if (topR.distance(now) < 5)
 			position = GrabPosition.TOP_RIGHT;
-		
+
 		topR.translate(0, (int) allowed.getHeight());
-		if(topR.distance(now)<5) 
+		if (topR.distance(now) < 5)
 			position = GrabPosition.BOTTOM_RIGHT;
-		
-		if(position == GrabPosition.BOTTOM) {
-			if(now.getX()-allowed.getX() <5)
+
+		if (position == GrabPosition.BOTTOM) {
+			if (now.getX() - allowed.getX() < 5)
 				position = GrabPosition.LEFT;
-			if(now.getY()-allowed.getY()<5)
+			if (now.getY() - allowed.getY() < 5)
 				position = GrabPosition.TOP;
-			if((allowed.getX()+allowed.getWidth())-now.getX() < 5)
+			if ((allowed.getX() + allowed.getWidth()) - now.getX() < 5)
 				position = GrabPosition.RIGHT;
 		}
-		if(position != GrabPosition.BOTTOM) {
-			if(release) {
+		if (position != GrabPosition.BOTTOM) {
+			if (release) {
 				fit(position, allowed);
 				preview.setSize(0, 0);
-			}else {
+			} else {
 				showPreview(position, allowed);
 			}
-		}else {
+		} else {
 			preview.setSize(0, 0);
 		}
 	}
+
 	/**
 	 * <b>Description :</b><br>
 	 * 
 	 * @author Nils Brugger
 	 * @version 2018-08-09
 	 * @param position
-	 * @param allowed 
+	 * @param allowed
 	 */
 	private void fit(GrabPosition position, Rectangle allowed) {
 		switch (position) {
 		case BOTTOM_LEFT:
-			theme.getFrame().setLocation(0, (int) (allowed.getHeight()/2));
-			theme.getFrame().setSize((int) allowed.getWidth()/2, (int) (allowed.getHeight()/2));
+			theme.getFrame().setLocation(0, (int) (allowed.getHeight() / 2));
+			theme.getFrame().setSize((int) allowed.getWidth() / 2, (int) (allowed.getHeight() / 2));
 			break;
 		case BOTTOM_RIGHT:
-			theme.getFrame().setSize((int) allowed.getWidth()/2, (int) (allowed.getHeight()/2));
-			theme.getFrame().setLocation((int) allowed.getWidth()/2, (int) (allowed.getHeight()/2));
+			theme.getFrame().setSize((int) allowed.getWidth() / 2, (int) (allowed.getHeight() / 2));
+			theme.getFrame().setLocation((int) allowed.getWidth() / 2, (int) (allowed.getHeight() / 2));
 			break;
 		case LEFT:
 			theme.getFrame().setLocation(0, 0);
-			theme.getFrame().setSize((int) (allowed.getWidth()/2), (int)allowed.getHeight());
+			theme.getFrame().setSize((int) (allowed.getWidth() / 2), (int) allowed.getHeight());
 			break;
 		case RIGHT:
-			theme.getFrame().setLocation((int) (allowed.getWidth()/2), 0);
-			theme.getFrame().setSize((int) (allowed.getWidth()/2), (int)allowed.getHeight());
+			theme.getFrame().setLocation((int) (allowed.getWidth() / 2), 0);
+			theme.getFrame().setSize((int) (allowed.getWidth() / 2), (int) allowed.getHeight());
 			break;
 		case TOP:
 			theme.getFrame().setBounds(allowed);
 			break;
 		case TOP_LEFT:
 			theme.getFrame().setLocation(0, 0);
-			theme.getFrame().setSize((int) allowed.getWidth()/2, (int) (allowed.getHeight()/2));
+			theme.getFrame().setSize((int) allowed.getWidth() / 2, (int) (allowed.getHeight() / 2));
 			break;
 		case TOP_RIGHT:
-			theme.getFrame().setLocation((int) (allowed.getWidth()/2), 0);
-			theme.getFrame().setSize((int) allowed.getWidth()/2, (int) (allowed.getHeight()/2));
+			theme.getFrame().setLocation((int) (allowed.getWidth() / 2), 0);
+			theme.getFrame().setSize((int) allowed.getWidth() / 2, (int) (allowed.getHeight() / 2));
 			break;
 		default:
 			return;
 		}
 	}
+
 	/**
 	 * <b>Description :</b><br>
 	 * 
@@ -177,39 +182,40 @@ public class MoveListener extends MouseAdapter {
 	 * @version 2018-08-09
 	 * @param position
 	 */
-	private void showPreview(GrabPosition position,Rectangle allowed) {
+	private void showPreview(GrabPosition position, Rectangle allowed) {
 		switch (position) {
 		case BOTTOM_LEFT:
-			preview.setLocation(0, (int) (allowed.getHeight()/2));
-			preview.setSize((int) allowed.getWidth()/2, (int) (allowed.getHeight()/2));
+			preview.setLocation(0, (int) (allowed.getHeight() / 2));
+			preview.setSize((int) allowed.getWidth() / 2, (int) (allowed.getHeight() / 2));
 			break;
 		case BOTTOM_RIGHT:
-			preview.setSize((int) allowed.getWidth()/2, (int) (allowed.getHeight()/2));
-			preview.setLocation((int) allowed.getWidth()/2, (int) (allowed.getHeight()/2));
+			preview.setSize((int) allowed.getWidth() / 2, (int) (allowed.getHeight() / 2));
+			preview.setLocation((int) allowed.getWidth() / 2, (int) (allowed.getHeight() / 2));
 			break;
 		case LEFT:
 			preview.setLocation(0, 0);
-			preview.setSize((int) (allowed.getWidth()/2), (int)allowed.getHeight());
+			preview.setSize((int) (allowed.getWidth() / 2), (int) allowed.getHeight());
 			break;
 		case RIGHT:
-			preview.setLocation((int) (allowed.getWidth()/2), 0);
-			preview.setSize((int) (allowed.getWidth()/2), (int)allowed.getHeight());
+			preview.setLocation((int) (allowed.getWidth() / 2), 0);
+			preview.setSize((int) (allowed.getWidth() / 2), (int) allowed.getHeight());
 			break;
 		case TOP:
 			preview.setBounds(allowed);
 			break;
 		case TOP_LEFT:
 			preview.setLocation(0, 0);
-			preview.setSize((int) allowed.getWidth()/2, (int) (allowed.getHeight()/2));
+			preview.setSize((int) allowed.getWidth() / 2, (int) (allowed.getHeight() / 2));
 			break;
 		case TOP_RIGHT:
-			preview.setLocation((int) (allowed.getWidth()/2), 0);
-			preview.setSize((int) allowed.getWidth()/2, (int) (allowed.getHeight()/2));
+			preview.setLocation((int) (allowed.getWidth() / 2), 0);
+			preview.setSize((int) allowed.getWidth() / 2, (int) (allowed.getHeight() / 2));
 			break;
 		default:
 			return;
 		}
 	}
+
 	private void moveWindow(Point now) {
 		int xdif = now.x - lastPos.x;
 		int ydif = now.y - lastPos.y;
@@ -217,17 +223,18 @@ public class MoveListener extends MouseAdapter {
 		windowPos.translate(xdif, ydif);
 		theme.getFrame().setLocation(windowPos);
 	}
+
 	private void stayInArea(Point now, InputEvent e) {
 		Rectangle allowed = theme.getFrame().getMaximizedSize();
-		if(!allowed.contains(now)) {
-			if(preventer != null) {
-				if(allowed.contains(now.x, lastPos.y)) {
+		if (!allowed.contains(now)) {
+			if (preventer != null) {
+				if (allowed.contains(now.x, lastPos.y)) {
 					preventer.mouseMove(now.x, lastPos.y);
 					now.y = lastPos.y;
-				}else if(allowed.contains(lastPos.x, now.y)) {
+				} else if (allowed.contains(lastPos.x, now.y)) {
 					preventer.mouseMove(lastPos.x, now.y);
 					now.x = lastPos.x;
-				}else {
+				} else {
 					preventer.mouseMove(lastPos.x, lastPos.y);
 					now.setLocation(lastPos.x, lastPos.y);
 				}
@@ -235,10 +242,12 @@ public class MoveListener extends MouseAdapter {
 			e.consume();
 		}
 	}
+
 	@Override
 	public void mouseReleased(MouseEvent e) {
+		if (e.getButton() != MouseEvent.BUTTON1)
+			return;
 		borderCheck(e.getLocationOnScreen(), true);
 		lastPos = null;
 	}
 }
-
