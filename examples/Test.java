ae87d036190fce1818f9e1180d035444a055a7f9
Nils Brugger <34156963+nbrugger-tgm@users.noreply.github.com>
2018-08-13 21:12:57 +0200

    Test Case 6
    Sizing of Root and Content Pane

--- examples/Test.java
+++ examples/Test.java
@@ -2,9 +2,12 @@ import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Rectangle;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.net.URL;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -15,6 +18,7 @@ import com.niton.themes.OSXTheme.Size;
 import com.niton.themes.base.ResizeableTheme;
 
 import java.awt.Color;
+import java.awt.FlowLayout;
 
 /**
  * This is the Test Class
@@ -23,13 +27,55 @@ import java.awt.Color;
  */
 public class Test {
 	public static void main(String[] args) {
-		testOSXTheme();
-		testHeader();
-		testContent();
-		testFlatTheme();
-		testMovingAndResizing();
+//		testOSXTheme();
+//		testHeader();
+//		testContent();
+//		testFlatTheme();
+//		testMovingAndResizing();
+		testSizing();
 	}
 	
+	/**
+	 * <b>Description :</b><br>
+	 * 
+	 * @author Nils Brugger
+	 * @version 2018-08-13
+	 */
+	private static void testSizing() {
+		CustomFrame frame = new CustomFrame(new FlatDarkTheme());
+		frame.setSize(300,300);
+		frame.setVisible(true);
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.GREEN);
+		frame.getContentPane().setLayout(new GridLayout());
+		frame.getContentPane().add(panel);
+		frame.getContentPane().setBackground(Color.RED);
+		frame.repaint();
+		
+		frame.addComponentListener(new ComponentAdapter() {
+			
+			@Override
+			public void componentResized(ComponentEvent e) {
+				new Thread(new Runnable() {
+					
+					@Override
+					public void run() {
+						try {
+							Thread.sleep(2000);
+						} catch (InterruptedException e) {
+						}
+						System.out.println("Panel Size = "+panel.getSize());
+						System.out.println("Frame Size = "+frame.getSize());
+						System.out.println("Content Pane Size = "+frame.getContentPane().getSize());
+						System.out.println("Theme content Size = "+frame.getTheme().getContentSize().getSize());
+						System.out.println("Layout : "+frame.getRootPane().getLayout());
+					}
+				}).start();
+				
+			}
+		});
+	}
+
 	/**
 	 * <b>Description :</b><br>
 	 * 
@@ -68,9 +114,9 @@ public class Test {
 		CustomFrame frame = new CustomFrame(new FlatDarkTheme());
 		frame.setSize(300,300);
 		frame.setVisible(true);
-		JLabel panel = new JLabel("XNXX");
-		panel.setBackground(Color.GREEN);
-		frame.getContentPane().setLayout(new GridLayout());
+		JButton	 panel = new JButton("I biz");
+//		panel.setBackground(Color.GREEN);
+		frame.getContentPane().setLayout(new FlowLayout());
 		frame.getContentPane().add(panel);
 		frame.repaint();
 	}
